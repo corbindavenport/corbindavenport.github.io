2b9e0e97e7b56e671c37b12cc2d5f9e12ea05ba2
Corbin Davenport <me@corbin.io>
2024-06-03 14:10:37 -0400

    Add nofollow to external links

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 // Open external links in new tab
-document.querySelectorAll('a').forEach(function (linkEl) {
+document.querySelectorAll('main a').forEach(function (linkEl) {
     const link = new URL(linkEl.href).href;
     if (!link.includes(window.location.hostname)) {
         linkEl.setAttribute('target', '_blank');
+        linkEl.setAttribute('rel', 'nofollow')
     }
 })
